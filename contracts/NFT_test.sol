113435389f3031a41b9c4db5233f5e015ace5baa
Adam-O-Phillips <Dolohov.Ivan.2007@gmail.com>
2021-11-24 07:12:06 +0900

    commit for some update.

--- contracts/NFT_test.sol
+++ contracts/NFT_test.sol
@@ -14,7 +14,7 @@ contract NFT_test is ERC721URIStorage {
     Counters.Counter private _tokenIds;
     address contractAddress;
 
-    constructor (address marketplaceAddress) ERC721("Metaverse TOkens", "METT") {
+    constructor (address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
         contractAddress = marketplaceAddress;
     }
 
